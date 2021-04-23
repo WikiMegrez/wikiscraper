ab7e89d60d2ab771c12ea5119fe4b30f5c77e3a1
Yichen Xu <yichen.x@outlook.com>
2021-04-23 12:13:50 +0800

    Add parser for Wiki entries

---
+++ src/main/scala/wikiscraper/WikiParser.scala
@@ -0,0 +1,10 @@
+package wikiscraper
+
+import org.jsoup.nodes._
+
+/**
+ * Parsing HTML document to [[WikiEntry]].
+ */
+object WikiParser {
+  def parseWikiEntry(doc: Document): WikiEntry = ???
+}
