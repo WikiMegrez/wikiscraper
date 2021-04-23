01aa40c3f58aabf5209d84f37e6a0fe5d425f20d
Yichen Xu <yichen.x@outlook.com>
2021-04-23 12:13:41 +0800

    Add WikiEntry data structure

---
+++ src/main/scala/wikiscraper/WikiEntry.scala
@@ -0,0 +1,21 @@
+package wikiscraper
+
+/**
+ * Wiki entry in Wikipedia.
+ * @param title
+ * @param entryId
+ * @param mainDesc
+ * @param mainImage
+ * @param outlinks
+ * @param content
+ * @param otherImages
+ */
+case class WikiEntry(title: String,
+                     entryId: String,
+                     mainDesc: String,
+                     mainImage: String,
+                     outlinks: List[String],
+                     content: String,
+                     otherImages: List[String])
+
+object WikiEntry
